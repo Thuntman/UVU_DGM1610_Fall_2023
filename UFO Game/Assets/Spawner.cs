eeb17aad9bc102653a3eec5186eb250385ef1991
Tyler Hunt <10936846@uvu.edu>
2023-10-10 20:05:30 -0600

    Attempted to add item

---
+++ UFO Game/Assets/Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+public GameObject [] item;
+[SerializeField]
+private float spawnRangeX = 17.0f;
+[SerializeField]
+private float spawnPosZ;
+private float startDelay = 1f;
+private float spawnInterval = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+InvokeRepeating("SpawnRandomItem", startDelay, spawnInterval);
+        
+    }
+
+    // Update is called once per frame
+    void SpawnRandomItem()
+    {
+     Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnPosZ);
+int itemIndex = Random.Range(0, item.Length);
+Instantiate(item[itemIndex], spawnPos, item[itemIndex].transform.rotation);   
+    }
+}
