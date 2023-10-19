b474c94ad5730480474aa98ed5c67c11ae552700
Tyler Hunt <10936846@uvu.edu>
2023-10-18 23:28:46 -0600

    Finished adding score and "Game Over!"

--- UFO Game/Assets/GameManager.cs
+++ UFO Game/Assets/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 public bool isGameOver;
-private GameObject gameOverText;
+public GameObject gameOverText;
     // Start is called before the first frame update
     void Awake()
     {
