85549594f16c2a591e7acfd78c8e9da7cdbe1f26
Tyler Hunt <10936846@uvu.edu>
2023-10-25 19:07:34 -0600

    Added IntData

---
+++ Unit 5 project/Assets/Scripts/IntData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Int Data", menuName = "Int Data")]
+
+public class IntData : ScriptableObject
+{
+	[SerializeField]
+private int value;
+   public int Value
+	{
+		get 
+		{
+			return value;
+		}
+		set 
+		{
+			this.value = value;
+		}
+	}
+public void UpdateValue(int num)
+	{
+		value += num;
+	}
+}
