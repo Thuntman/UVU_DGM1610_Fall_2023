01f487e759adb3b67ad1684e26dccc4d35cf8565
Tyler Hunt <10936846@uvu.edu>
2023-10-26 18:06:38 -0600

    Added Sound Effects

--- UFO Game/Assets/CollisionDetect.cs
+++ UFO Game/Assets/CollisionDetect.cs
@@ -7,10 +7,15 @@ public class CollisionDetect : MonoBehaviour
  private ScoreManager scoreManager; // A variable to hold the reference to the scoremanager
     public int scoreToGive;
 
+private AudioSource explosionAudio;
+    public AudioClip explosionSound;
+
+
     // Start is called before the first frame update
     void Start()
     {
         scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); // Reference scoremanager
+explosionAudio = GetComponent<AudioSource>();
         
     }
 
@@ -19,6 +24,6 @@ public class CollisionDetect : MonoBehaviour
         scoreManager.IncreaseScore(scoreToGive); // Increase Score 
         Destroy(other.gameObject); //Destroy the other game object it hits
         Destroy(gameObject); // Destroy this game object 
-        
+        explosionAudio.PlayOneShot(explosionSound, 1f);
     }
 }
